fb0433b83f100605d64cf18b9bd644839124109e
Kejin Zhou <kejin.zhou@aalto.fi>
2024-07-26 11:52:35 +0300

    adds main.py template file

---
+++ templates/main.py
@@ -0,0 +1,9 @@
+
+
+def main() -> bool:
+    print("Hello World.")
+    return True
+
+# Running this file will call the main() function like an entrypoint as per convention familiar to C programs
+if __name__ == "__main__":
+    main()
